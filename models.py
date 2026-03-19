90d5dd4d67151f86cb97368e120b6788d43fdf68
loucia_laize <loucia.laize95@gmail.com>
2026-03-19 14:25:37 +0100

    files added

--- models.py
+++ models.py
@@ -1,6 +1,11 @@
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coloc.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
