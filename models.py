8f3648005c72b72edd40b7978d28bad3eb0f175e
loucia_laize <loucia.laize95@gmail.com>
2026-03-19 14:33:30 +0100

    models modified

--- models.py
+++ models.py
@@ -1,30 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///coloc.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nom = db.Column(db.String(50), nullable=False)
-    prenom = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    id       = db.Column(db.Integer, primary_key=True)
+    nom      = db.Column(db.String(50), nullable=False)
+    prenom   = db.Column(db.String(50), nullable=False)
+    email    = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    # Lien vers les tâches et dépenses
-    taches = db.relationship('Tache', backref='responsable', lazy=True) #backref pour faire le lien entre les tables
+    taches   = db.relationship('Tache', backref='responsable', lazy=True)
 
 class Depense(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titre = db.Column(db.String(100), nullable=False)
-    montant = db.Column(db.Float, nullable=False) # C'est ici qu'on gère le pot commun [cite: 1, 12]
-    payeur = db.Column(db.String(50), nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    id      = db.Column(db.Integer, primary_key=True)
+    titre   = db.Column(db.String(100), nullable=False)
+    montant = db.Column(db.Float, nullable=False)
+    payeur  = db.Column(db.String(50), nullable=False)
+    date    = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Tache(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titre = db.Column(db.String(100), nullable=False) # ex: CUISINE, MÉNAGE [cite: 61, 62]
-    assignee = db.Column(db.String(50), nullable=False)
-    faite    = db.Column(db.Boolean, default=False)
+    id          = db.Column(db.Integer, primary_key=True)
+    titre       = db.Column(db.String(100), nullable=False)
+    assignee    = db.Column(db.String(50), nullable=False)
+    faite       = db.Column(db.Boolean, default=False)
+    user_id     = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
