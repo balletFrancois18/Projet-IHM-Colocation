5a0f33fe248411cb203a86e27be4b8403830aa9b
loucia_laize <loucia.laize95@gmail.com>
2026-03-20 10:09:00 +0100

    liens menu

--- routes/depenses.py
+++ routes/depenses.py
@@ -9,18 +9,22 @@ depenses_bp = Blueprint('depenses', __name__)
 # Blueprint = façon de découper l'app en morceaux
 
 
-@depenses_bp.route('/depenses')
+@depenses_bp.route('/depenses') #lit toutes les dépenses dans la BDD et calcule le total
 def liste_depenses():
-    return redirect(url_for('depenses.html'))
+    depenses = Depense.query.all()
+    total = sum(d.montant for d in depenses)
+    return render_template('depenses.html', depenses=depenses, total=total)
 
+
+ #affiche le formulaire d'ajout de dépense et la liste des dépenses existantes
 @depenses_bp.route('/depenses/ajouter', methods=['GET', 'POST'])
 def ajouter_depense():
-    if request.method == 'POST':
-        titre   = request.form['']
-        montant = float(request.form[''])
-        payeur  = request.form['']
+    if request.method == 'POST': #enregistre dans la BDD et redirige vers la liste des dépenses
+        titre   = request.form['titre']
+        montant = float(request.form['montant'])
+        payeur  = request.form['payeur']
         nouvelle = Depense(titre=titre, montant=montant, payeur=payeur)
         db.session.add(nouvelle)
         db.session.commit()
         return redirect(url_for('depenses.liste_depenses'))
-    return render_template('ajouter_depenses.html')
+    return render_template('depenses.html', depenses=Depense.query.all(), total=0)
