9e763ec2bfd87eb03388a0a652d12ed1a63675b2
loucia_laize <loucia.laize95@gmail.com>
2026-03-21 15:25:35 +0100

    ajout opérations pot commun montant, depensé , reste

--- static/script.js
+++ static/script.js
@@ -9,23 +9,23 @@
 
  */
 
-function ouvrirModalModifier(id, titre, montant, payeur) {
-  document.getElementById('modal-titre').textContent = 'Modifier une charge';
-  document.getElementById('modal-form').action = '/depenses/modifier/' + id;
-  document.getElementById('input-titre').value   = titre;
-  document.getElementById('input-montant').value = montant;
-  document.getElementById('input-payeur').value  = payeur;
-  document.getElementById('modal').classList.add('active');
-}
+// function ouvrirModalModifier(id, titre, montant, payeur) {
+//   document.getElementById('modal-titre').textContent = 'Modifier une charge';
+//   document.getElementById('modal-form').action = '/depenses/modifier/' + id;
+//   document.getElementById('input-titre').value   = titre;
+//   document.getElementById('input-montant').value = montant;
+//   document.getElementById('input-payeur').value  = payeur;
+//   document.getElementById('modal').classList.add('active');
+// }
 
-function ouvrirModal(type) {
-  document.getElementById('modal-titre').textContent = 'Ajouter une charge';
-  document.getElementById('modal-form').action = "{{ url_for('depenses.ajouter_depense') }}";
-  document.getElementById('input-titre').value   = '';
-  document.getElementById('input-montant').value = '';
-  document.getElementById('input-payeur').value  = '';
-  document.getElementById('modal').classList.add('active');
-}
+// function ouvrirModal(type) {
+//   document.getElementById('modal-titre').textContent = 'Ajouter une charge';
+//   document.getElementById('modal-form').action = "{{ url_for('depenses.ajouter_depense') }}";
+//   document.getElementById('input-titre').value   = '';
+//   document.getElementById('input-montant').value = '';
+//   document.getElementById('input-payeur').value  = '';
+//   document.getElementById('modal').classList.add('active');
+// }
 
 
 /*calendrier*/
